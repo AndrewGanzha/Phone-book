3d0a22c2a4760e9e5bad5dddca42c3fbc8673e27
AndrewGanzha <ganzha-2002@mail.com>
2023-08-31 15:14:34 +0300

    Add search and correct delete element

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 import eventBus from "./eventBus";
 
 interface Contact {
+  id: string;
   name: string;
   phone: number;
   email: string;
@@ -20,15 +21,28 @@ export const useContactsStore = defineStore("contactDB", {
       localStorage.setItem("contactDB", JSON.stringify(newContacts));
     },
     setContact({ name, phone, email }: Contact) {
-      let newContact = { name: name, phone: phone, email: email };
+      let newContact = {id: crypto.randomUUID(), name: name, phone: phone, email: email };
       this.contacts.push(newContact);
       const updateContact = JSON.stringify(this.contacts);
       localStorage.setItem("contactDB", updateContact);
       eventBus.emit("contacts-updated");
     },
-    removeContact(index: number) {
-      this.contacts.splice(index, 1);
-      this.updateContacts(this.contacts);
+    removeContact(id: string) {
+      const index = this.contacts.findIndex(item => item.id === id)
+      if(index !== -1) {
+        this.contacts.splice(index, 1);
+        this.updateContacts(this.contacts);
+      }
     },
+    searchContacts(query: string) {
+      query = query.toLowerCase();
+      return this.contacts.filter(contact => {
+        return (
+          contact.name.toLowerCase().includes(query) ||
+          contact.email.toLowerCase().includes(query) ||
+          contact.phone.toString().toLowerCase().includes(query)
+        );
+      });
+    }
   },
 });
